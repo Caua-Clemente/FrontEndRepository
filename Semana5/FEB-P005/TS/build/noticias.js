97b329895df98465fa984373e62b82f710e3aed2
Caua-Clemente <cauaclementelima@gmail.com>
2023-12-21 21:40:09 -0300

    Colocando noticias em menu

--- Semana5/FEB-P005/TS/build/noticias.js
+++ Semana5/FEB-P005/TS/build/noticias.js
@@ -9,15 +9,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 let artigos;
-function getNews2() {
+function getNews1() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const apiKey = 'd8787b010d992ca0554a93011a8d537e';
-            const q = 'universidade';
+            const q = '(universidade AND tecnologia AND educação) OR (universidade AND tecnologia)';
             const lang = 'pt';
             const country = 'br';
             const max = '10';
             const url = `https://gnews.io/api/v4/search?q=${q}&lang=${lang}&country=${country}&max=${max}&apikey=${apiKey}`;
+            console.log(url);
             const response = yield fetch(url);
             if (!response.ok) {
                 throw new Error('Erro ao buscar notícias.');
@@ -31,11 +32,54 @@ function getNews2() {
     });
 }
 // Utilização da função para obter as notícias
-getNews2()
+getNews1()
     .then((news) => {
     console.log(news);
-    artigos = news;
+    artigos = news.articles;
+    const maxLength = 40;
+    let menus = [];
+    menus.push("menu-titulo-1");
+    menus.push("menu-titulo-2");
+    menus.push("menu-titulo-3");
+    let numAux = 0;
+    menus.forEach(menu => {
+        let elemento = document.getElementById(menu);
+        let titulo = artigos[numAux].title;
+        if (titulo.length > maxLength) {
+            titulo = titulo.substring(0, maxLength) + "...";
+        }
+        elemento.innerHTML = `<a href='${artigos[numAux++].source.url}'>${titulo}</a>`;
+    });
 })
     .catch((error) => {
     console.error(error);
 });
+/*
+ let menuT1 = document.getElementById("menu-titulo-1")
+ let menuT2 = document.getElementById("menu-titulo-2")
+ let menuT3 = document.getElementById("menu-titulo-3")
+
+ if(menuT1){
+   let titulo : string = artigos[0].title;
+   if(titulo.length > maxLength){
+     titulo = titulo.substring(0, maxLength) + "...";
+   }
+   menuT1.innerHTML = `<a href='${artigos[0].source.url}'>${titulo}</a>`
+ }
+
+ if(menuT2){
+   let titulo : string = artigos[1].title;
+   if(titulo.length > maxLength){
+     titulo = titulo.substring(0, maxLength) + "...";
+   }
+   menuT2.innerHTML = `<a href='${artigos[1].source.url}'>${titulo}</a>`
+ }
+
+ if(menuT3){
+   let titulo : string = artigos[2].title;
+   if(titulo.length > maxLength){
+     titulo = titulo.substring(0, maxLength) + "...";
+   }
+   menuT3.innerHTML = `<a href='${artigos[2].source.url}'>${titulo}</a>`
+ }
+})*/ 
--- Semana5/FEB-P005/TS/src/noticias.ts
+++ Semana5/FEB-P005/TS/src/noticias.ts
@@ -1,15 +1,15 @@
 let artigos : any;
 
-async function getNews2(): Promise<any> {
+async function getNews1(): Promise<any> {
   try{
     const apiKey : string = 'd8787b010d992ca0554a93011a8d537e';
-    const q : string = 'universidade';
+    const q : string = '(universidade AND tecnologia AND educação) OR (universidade AND tecnologia)';
     const lang : string = 'pt';
     const country : string = 'br';
     const max : string = '10';
 
     const url = `https://gnews.io/api/v4/search?q=${q}&lang=${lang}&country=${country}&max=${max}&apikey=${apiKey}`;
-    
+    console.log(url)
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -23,13 +23,62 @@ async function getNews2(): Promise<any> {
     throw new Error(`Erro na requisição: ${(error as Error).message}`);
   }
 }
-  
+
+
+
   // Utilização da função para obter as notícias
-  getNews2()
+  getNews1()
     .then((news) => {
       console.log(news);
-      artigos = news;
+      artigos = news.articles;
+      const maxLength : number = 40;
+
+      let menus : string[] = [];
+      menus.push("menu-titulo-1");
+      menus.push("menu-titulo-2");
+      menus.push("menu-titulo-3");
+
+      let numAux : number = 0;
+      menus.forEach(menu  => {
+        let elemento : any = document.getElementById(menu)
+
+        let titulo : string = artigos[numAux].title;
+        if(titulo.length > maxLength){
+          titulo = titulo.substring(0, maxLength) + "...";
+        }
+        elemento.innerHTML = `<a href='${artigos[numAux++].source.url}'>${titulo}</a>`
+      });
     })
     .catch((error) => {
       console.error(error);
     });
+
+     /*
+      let menuT1 = document.getElementById("menu-titulo-1")
+      let menuT2 = document.getElementById("menu-titulo-2")
+      let menuT3 = document.getElementById("menu-titulo-3")
+
+      if(menuT1){
+        let titulo : string = artigos[0].title;
+        if(titulo.length > maxLength){
+          titulo = titulo.substring(0, maxLength) + "...";
+        }
+        menuT1.innerHTML = `<a href='${artigos[0].source.url}'>${titulo}</a>`
+      }
+
+      if(menuT2){
+        let titulo : string = artigos[1].title;
+        if(titulo.length > maxLength){
+          titulo = titulo.substring(0, maxLength) + "...";
+        }
+        menuT2.innerHTML = `<a href='${artigos[1].source.url}'>${titulo}</a>`
+      }
+
+      if(menuT3){
+        let titulo : string = artigos[2].title;
+        if(titulo.length > maxLength){
+          titulo = titulo.substring(0, maxLength) + "...";
+        }
+        menuT3.innerHTML = `<a href='${artigos[2].source.url}'>${titulo}</a>`
+      }
+    })*/
