af5d14c72952047cd10850a08574273669a04fbf
Caua-Clemente <cauaclementelima@gmail.com>
2024-01-29 15:00:03 -0300

    Iniciando a atividade FEB007

---
+++ Semana7/FEB-P007/JReader/src/app/propriedades/propriedades.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-propriedades',
+  templateUrl: './propriedades.component.html',
+  styleUrl: './propriedades.component.css'
+})
+export class PropriedadesComponent {
+
+}
---
+++ Semana7/FEB-P007/JReader/src/app/valor-propriedade/valor-propriedade.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valor-propriedade',
+  templateUrl: './valor-propriedade.component.html',
+  styleUrl: './valor-propriedade.component.css'
+})
+export class ValorPropriedadeComponent {
+
+}
