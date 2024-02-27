d1083eac1aabe51559c54099998857f37564b5e5
Caua-Clemente <cauaclementelima@gmail.com>
2024-02-27 08:24:48 -0300

    Criando branch da semana 8

--- Semana7/FEB-P007/JReader/src/app/shared/directives/text-content-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextContentDirectiveDirective } from './text-content-directive.directive';
-
-describe('TextContentDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TextContentDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Semana7/FEB-P007/JReader/src/app/shared/directives/title-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TitleDirectiveDirective } from './title-directive.directive';
-
-describe('TitleDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TitleDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
