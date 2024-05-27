e5116902d044c3653b23a6622ffb638494c20cc5
Caua-Clemente <cauaclementelima@gmail.com>
2024-05-27 18:17:17 -0300

    Finalizando P010

--- FEBP010/src/app/app.component.ts
+++ FEBP010/src/app/app.component.ts
@@ -10,15 +10,22 @@ export class AppComponent {
   title = 'FEBP010';
 
   listaItens = signal<Item[]>([
-    { id: 0, name: 'carro', price: 1, quantity: 1, imageURL: '../../../assets/images/car_image.jiff', marcado: 0}, 
-    { id: 1, name: 'CARRETAAAAAA', price: 100000, quantity: 1, imageURL: '../../../assets/images/car_image.jiff', marcado: 0}, 
-    { id: 2, name: 'moriusro', price: 1000, quantity: 2, imageURL: '../../../assets/images/car_image.jiff', marcado: 1},
-    { id: 3, name: 'moriusro', price: 1000, quantity: 2, imageURL: 'ads', marcado: 1} 
+    { id: 0, name: 'Escrivaninha', price: 400, quantity: 0, imageURL: '../assets/images/escrivaninha.jpg', marcado: 0}, 
+    { id: 1, name: 'Mouse RGB', price: 200, quantity: 0, imageURL: '../assets/images/mouse_gamer.jpg', marcado: 0}, 
+    { id: 2, name: 'Notebook Daten', price: 1800, quantity: 0, imageURL: '../assets/images/notebook_daten.jpg', marcado: 0},
+    { id: 3, name: 'Suporte para notebook', price: 80, quantity: 0, imageURL: '../assets/images/suporte_notebook.jpg', marcado: 0},
+    { id: 4, name: 'Teclado ABNT2', price: 250, quantity: 0, imageURL: '../assets/images/teclado_abnt2.jpg', marcado: 0} 
   ]);
 
   subTotal = computed(() => 
     this.listaItens().reduce((acc, item) => acc + (item.price * item.quantity * item.marcado), 0)
   )
+
+  subQuantidade = computed(() => 
+    this.listaItens().reduce((acc, item) => acc + (item.quantity * item.marcado), 0)
+  )
+
+  objetoSignal = signal({quantidade: this.subQuantidade(), valor: this.subTotal()});
 
   recebendoQuantidade(event: {id: number, quantidade: number}){
     const valorAtual = this.listaItens(); 
@@ -35,6 +42,12 @@ export class AppComponent {
     
     valorNovo[event.id] = { ...valorNovo[event.id], marcado: event.marcado };
     
+    this.listaItens.set(valorNovo);
+  }
+
+  desmarcarTodos() {
+    const valorAtual = this.listaItens();
+    const valorNovo = valorAtual.map(item => ({ ...item, marcado: 0 }));
     this.listaItens.set(valorNovo);
   }
 }
